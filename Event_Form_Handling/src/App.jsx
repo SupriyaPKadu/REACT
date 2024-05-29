3919b9e1aa8ea92a0daed7c751effb559d955eb0
Classic_User <supriyapkadu@gmail.com>
2024-05-29 18:37:56 +0530

    commit 1

--- Event_Form_Handling/src/App.jsx
+++ Event_Form_Handling/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MyForm from './components/MyForm'
+import Event_Handle from './components/EventBind/Event_Handle'
+import Event_Handle from './components/EventBind/Event_Handle'
 
 function App() {
   const [count, setCount] = useState(0)
